1578d6555b6e14f13759ec9fe734ff35688e3cca
Nicole Mumo <nicolemumo3@gmail.com>
2025-07-04 10:02:04 +0300

    Farmer page working

--- server/src/routes/productRoutes.js
+++ server/src/routes/productRoutes.js
@@ -7,8 +7,8 @@ const authMiddleware = require("../middleware");
 router.get("/products", productController.getProducts);
 router.get("/products/:id", productController.getProductById);
 router.post("/products", authMiddleware, productController.addProduct);
-router.put("/products/:id", productController.updateProduct);
-router.patch("/products/:id", productController.updateProduct);
-router.delete("/products/:id", productController.deleteProduct);
+router.put("/products/:id", authMiddleware, productController.updateProduct);
+router.patch("/products/:id", authMiddleware, productController.updateProduct);
+router.delete("/products/:id", authMiddleware, productController.deleteProduct);
 
 module.exports = router;
